3267a961367eae27cf70c8991a8c2f92a0c586a7
Scott Kent <swtkent@gmail.com>
2025-01-19 23:27:47 -0700

    updated method of calculating separation and functions that go into that

--- lunar_transfer.py
+++ lunar_transfer.py
@@ -1,19 +1,64 @@
 import numpy as np
+from numpy.typing import NDArray
 from scipy.optimize import minimize
 
 # Assumed Constants
 MU_MOON = 4.9048695e12  # Gravitational parameter of Moon (m^3/s^2)
 MOON_RADIUS = 1737.4e3  # Radius of Moon (m)
 
-# Initial orbit parameters
-PERILUNE_ALT = 100e3  # m
-APOLUNE_ALT = 10000e3  # m
-TARGET_SEPARATION = 10e3 # m
 
-
-def orbital_period(a: float, mu: float) -> float:
+def orbit_shape_from_altitudes(h_p, h_a, radius):
     """
-    Compute the orbital period around an object with SGP value of mu and a semi-major axis of a
+    Compute the semi-major axis and eccentricity given peri- and apo- apsis altitudes and celestial body's radius
+
+    Parameters
+    h_p: Periapsis altitude (m)
+    h_a: Apoapsis altitude (m)
+    radius: Celestial body radius (m)
+
+    Returns
+    a: Semi-major axis (m)
+    e: Eccentricity (unitless)
+    """
+    r_p = radius + h_p  # Periapsis radius
+    r_a = radius + h_a  # Apoapsis radius
+    a = (r_p + r_a) / 2  # Semi-major axis
+    e = 1 - r_p / a  # Eccentricity
+    return a, e
+
+
+def mean_motion(a: float, mu: float) -> float:
+    """
+    Compute the mean motion given semi-major axis and standard gravitational parameter
+
+    Parameters
+    a: Semi-major axis of the initial orbit (m)
+    mu: Standard gravitational parameter (SGP) (m^3/s^2)
+
+    Returns
+    n: Mean motion of orbit (rad/s)
+    """
+    n = np.sqrt(mu / a**3)
+    return n
+
+
+def period_from_mm(n: float) -> float:
+    """
+    Compute period from mean motion
+
+    Parameters
+    n: Mean motion of orbit (rad/s)
+
+    Returns
+    period: Time to complete one orbit revolution (s)
+    """
+    period = 2 * np.pi / n
+    return period
+
+def period_from_sma(a: float, mu: float) -> float:
+    """
+    Compute the orbital period around an object with SGP value of mu and
+    semi-major axis of a
 
     Parameters
     a: Semi-major axis of the initial orbit (m)
@@ -22,14 +67,15 @@ def orbital_period(a: float, mu: float) -> float:
     Returns
     period: Time to complete one orbit revolution (s)
     """
-    
-    period = 2 * np.pi * np.sqrt(a**3 / mu) # s
+    n = mean_motion(a, mu)
+    period = period_from_mm(n)
     return period
 
 
 def velocity_at_radius(r: float, a: float, mu: float) -> float:    
     """
-    Compute orbital velocity at a distance r from the center for an orbit with semi-major axis a.
+    Compute orbital velocity at a distance r from the center for an orbit with
+    semi-major axis a
 
     Parameters
     r: Radius of satellite (m)
@@ -37,59 +83,194 @@ def velocity_at_radius(r: float, a: float, mu: float) -> float:
     mu: Standard gravitational parameter (SGP) (m^3/s^2)
 
     Returns
-    velocity: Velocity at given radius for orbit with semi-major axis a and SGP mu (m/s)
+    velocity: Velocity at given radius for orbit with semi-major axis a and
+        SGP mu (m/s)
     """
-
-    velocity = np.sqrt(mu * (2 / r - 1 / a))
+    specific_energy = -mu / (2 * a)
+    velocity = np.sqrt(2 * (specific_energy + mu / r))
     return velocity
 
 
-def sma_at_rv(r: float, v: float, mu: float) -> float:
+def eccentric_anomaly_from_mean_anomaly(M, e, tol=1e-8):
     """
-    Copmute the semi-major axis given distance r and velocity v aroundd central body with SGP mu
-    Reworked equation for solving velocity with same parameters
+    Solve for the eccentric anomaly given the mean anomaly using Newton's method.
 
     Parameters
-    r: Radius of satellite (m)
-    v: Velocity of satellite (m/s)
-    mu: Standard gravitational parameter (SGP) (m^3/s^2)
+    M: Mean anomaly (rads)
+    e: Eccentricity (unitless)
+    tol: Tolerance for convergence
 
     Returns
-    a: Semi-major axis of the orbit (m)
+    E: Eccentric anomaly (rads)
     """
-    a = 1 / (2 / r - v**2 / mu)
-    return a
+    E = M if e < 0.8 else np.pi  # Initial guess
+    for _ in range(100):
+        delta = (E - e * np.sin(E) - M) / (1 - e * np.cos(E))
+        E -= delta
+        if abs(delta) < tol:
+            break
+    return E
 
 
-def separation_distance(a_initial:float, a_new: float, r_deploy: float) -> float:
+def true_anomaly_from_mean_anomaly(M, e):
+    """
+    Calculate the true anomaly given the mean anomaly.
+
+    Parameters
+    M: Mean anomaly (rads)
+    e: Eccentricity (unitless)
+    
+    Returns
+    true_anomaly: True anomaly (rads)
+    """
+    E = eccentric_anomaly_from_mean_anomaly(M, e)
+    true_anomaly = 2 * np.arctan2(
+        np.sqrt(1 + e) * np.sin(E / 2),
+        np.sqrt(1 - e) * np.cos(E / 2)
+    )
+    return true_anomaly
+
+
+def true_anomaly_at_time(a: float, e: float, M_start, dt: float, mu: float) -> float:
+    """
+    Calculate the true anomaly of an orbiting body at a given time.
+
+    Parameters
+    a: Semi-major axis (m)
+    e: Eccentricity (unitless)
+    dt: Time since M_start passage (s)
+    mu: Standard gravitational parameter (m^3/s^2)
+
+    Returns
+    true_anomaly: True anomaly (radians)
+    """
+    # Mean anomaly update
+    n = mean_motion(a, mu)
+    mean_anomaly = (M_start + n * dt) % (2 * np.pi)  # Wrap it to 0-2pi
+
+    # True anomaly
+    true_anomaly = true_anomaly_from_mean_anomaly(mean_anomaly, e)
+    return true_anomaly
+
+
+def radius_at_true_anomaly(a: float, e: float, theta: float) -> float:
+    """
+    Calculate the orbital radius at a given true anomaly.
+
+    Parameters
+    a: Semi-major axis (m)
+    e: Eccentricity (unitless)
+    theta: True anomaly (radians)
+    
+    Returns
+    r: Radius (m)
+    """
+    r = a * (1 - e**2) / (1 + e * np.cos(theta))
+    return r
+
+
+def orbit_shape_from_rv(r, v, mu):
+    """
+    Calculate the eccentricity of an orbit given radius, velocity, and the standard gravitational parameter
+
+    Parameters
+    r: Radius from the central body (m)
+    v: Orbital velocity at that radius (m/s)
+    mu: Gravitational parameter (m^3/s^2)
+
+    Returns
+    a: Semi-major axis (m)
+    e: Eccentricity (unitless)
+    """
+    specific_energy = v**2 / 2 - mu / r
+    a = -mu / (2 * specific_energy)
+    angular_momentum = r * v
+    e = np.sqrt(1 - (angular_momentum**2 / (mu * a)))
+    return a, e
+
+
+def separation_distance(position1: NDArray[np.float64], position2: NDArray[np.float64]) -> NDArray[np.float64]:
     """
     Compute the position difference after one revolution
 
     Parameters
-    a_initial: initial semi-major axis before separation (m)
-    a_new: new semi-major axis after separation (m)
-    r_deploy: the radius at which deployed from
+    position1: nxm matrix of positions (m)
+    position2 nxm matrix of positions (m)
 
     Returns
-    separation: separation distance
+    separation: separation distances (m)
     """
-    # Compute position difference after one revolution
-    theta_initial = 2 * np.pi
-    theta_new = theta_initial * (np.sqrt(a_initial**3 / a_new**3))
-    delta_theta = np.abs(theta_initial - theta_new)
-    separation = np.sqrt((r_deploy * np.sin(delta_theta))**2 + (r_deploy * (1 - np.cos(delta_theta)))**2)
+    separation = np.linalg.norm(position1 - position2)
     return separation
 
 
-def separation_after_one_revolution(dv: float, r: float, a_initial: float, mu: float) -> float:
+def position_2d(r, theta):
+    """
+    Compute 2d position given a radius and an angle
+
+    Parameters
+    r: Radius (m)
+    theta: Angle (rads)
+
+    Returns
+    position: 2-Long Vector of x and y positions (m)
+    """
+    unit_vector = np.array((np.cos(theta), np.sin(theta)))
+    position = r * unit_vector
+    return position
+
+
+def separation_after_time(dv, a_initial, e_initial, M_start, dt, mu):
+    """
+    Compute the separation distance between two satellites at a given time after deployment.
+
+    Parameters
+    dv: Delta-V (km/s)
+    a_initial: Semi-major axis of the initial orbit (m)
+    e_initial: Eccentricity of the initial orbit
+    M_start: Mean anomaly at separation (rads)
+    dt: Time after deployment to check separation (s)
+    mu: Standard gravitational parameter (m^3/s^2)
+    
+    Returns
+    separation: Separation distance (m)
+    """
+    # Orbital radius and velocity of the initial orbit and the starting true anomaly
+    true_anomaly = true_anomaly_from_mean_anomaly(M_start, e_initial)
+    r_deploy = radius_at_true_anomaly(a_initial, e_initial, true_anomaly)
+    v_initial = velocity_at_radius(a_initial, r_deploy, mu)
+
+    # Velocity after deployment
+    v_deploy = v_initial + dv
+
+    # Compute new orbit's semi-major axis and eccentricity
+    a_new, e_new = orbit_shape_from_rv(r_deploy, v_deploy, mu)
+
+    # True anomalies at the time of checking
+    theta_initial = true_anomaly_at_time(a_initial, e_initial, M_start, dt, mu)
+    theta_new = true_anomaly_at_time(a_new, e_new, M_start, dt, mu)
+
+    # Radii at the time of checking
+    r_undeployed = radius_at_true_anomaly(a_initial, e_initial, theta_initial)
+    r_deployed = radius_at_true_anomaly(a_new, e_new, theta_new)
+
+    # Separation distance
+    initial_position = position_2d(r_undeployed, theta_initial)
+    separated_position = position_2d(r_deployed, theta_new)
+    separation = separation_distance(initial_position, separated_position)
+    return separation
+
+
+def separation_after_one_revolution(dv: float, a_initial: float,
+        e_initial: float, M_start: float, mu: float) -> float:
     """
     Compute the separation distance after one orbital revolution
 
     Parameters
     dv: Delta-V for the separation (m/s)
-    r: radius at which separated (m)
-    v: velocity at radius at which separated (m/s)
     a_inital: Semi-major axis of original orbit (m)
+    e: Eccentricity (unitless)
+    M_start: Mean anomaly at separation time (rads)
     mu: Standard graviational parameter (m^3/s^2)
 
     Returns
@@ -97,57 +278,57 @@ def separation_after_one_revolution(dv: float, r: float, a_initial: float, mu: f
     """
 
     # Calculate original orbital period
-    T = orbital_period(a_initial, mu)
-
-    # Calculate the velocity at given position r
-    v = velocity_at_radius(r, a_initial, mu)
-
-    # Deployed satellite velocity
-    v_deployed = v + dv
-
-    # New semi-major axis and orbital period
-    a_new = sma_at_rv(r, v_deployed, mu)
-    T_new = orbital_period(a_new, mu)
+    T = period_from_sma(a_initial, mu)
 
     # Relative separation after one period
-    separation = separation_distance(a_initial, a_new, r)
+    separation = separation_after_time(dv, a_initial, e_initial, M_start, T, mu)
     return separation
 
 
-def optimize_dv(a_initial: float, r_deploy: float, target_separation: float, mu: float) -> float:
+def optimize_dv(a_initial: float, e_initial: float, M_start: float, mu: float, target_separation: float,
+        init_guess: float, bound: float) -> float:
     """
     Compute the minimum Delta-V to achieve the target separation after one revolution.
 
     Parameters
     a_initial: Semi-major axis of the initial orbit (m)
-    r_deploy: Deployment radius (m)
-    target_separation: Target separation distance (m)
+    e_initial: Eccentricity of initial orbit (unitless)
+    M: Mean anomaly at separation time (rads)
     mu: Standard Gravitational Parameter (m^3/s^2)
+    target_separation: Target separation distance (m)
+    init_guess: Initial value to assume for delta v
+    bound: upper value for positive/negative bounding of optimizer
 
     Returns
-    optimal_dv: Optimal delta-V (m/s)
+    optimal_dv: Optimal Delta-V (m/s)
     """
 
     def objective(dv):
         # objective function to put into optimizing function to minimize delta v
-        return abs(separation_after_one_revolution(dv, a_initial, r_deploy, mu) - target_separation)
+        return abs(separation_after_one_revolution(dv, a_initial, e_initial, M_start, mu)
+                - target_separation)
 
-    result = minimize(objective, x0=40.0, bounds=[(0, 1e3)])
+    result = minimize(objective, x0=init_guess, bounds=[(-bound, bound)])
     optimal_dv = result.x[0]
     return optimal_dv
 
 
 def main():
+    # Given initial orbit parameters
+    perilune_alt = 100e3  # m
+    apolune_alt = 10000e3  # m
+    target_separation = 10e3  # m
 
-    # Initial orbit parameters
-    a = (MOON_RADIUS + PERILUNE_ALT + MOON_RADIUS + APOLUNE_ALT) / 2  # Semi-major axis
-    r_p = MOON_RADIUS + PERILUNE_ALT  # Periapsis radius
-    r_a = MOON_RADIUS + APOLUNE_ALT  # Apoapsis radius
+    # values for guessing Delta V
+    init_guess = 50  # m
+    bound = 1e3  # m
 
-    dv_perilune = optimize_dv(a, r_p, TARGET_SEPARATION, MU_MOON)  # Velocity at periapsis
-    dv_apolune = optimize_dv(a, r_a, TARGET_SEPARATION, MU_MOON)  # Velocity at apoapsis
+    # Calculated initial orbit parameters
+    a, e = orbit_shape_from_altitudes(perilune_alt, apolune_alt, MOON_RADIUS)
 
-
+    # Optimized Delta-V's at peri- and apo- apses
+    dv_perilune = optimize_dv(a, e, 0, MU_MOON, target_separation, init_guess, bound)  # Velocity at periapsis
+    dv_apolune = optimize_dv(a, e, np.pi, MU_MOON, target_separation, init_guess, bound)  # Velocity at apoapsis
     print(f"Minimum Delta-V at Perilune: {dv_perilune:.3f} m/s")
     print(f"Minimum Delta-V at Apolune: {dv_apolune:.3f} m/s")
 
